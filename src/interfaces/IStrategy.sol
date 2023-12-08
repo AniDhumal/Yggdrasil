d058d627b9c2c0130a5c2df887c66cddd9ea8edb
epizeuxius <markericanthony@gmail.com>
2023-12-09 03:42:43 +0530

    Finished Strategy1 implementation

--- src/interfaces/IStrategy.sol
+++ src/interfaces/IStrategy.sol
@@ -3,7 +3,12 @@
 pragma solidity ^0.8.14;
 
 interface IStrategy {
-    function invest(address, uint256) external payable;
-    function devest(address, uint256) external;
+    event Invest(address indexed user, uint256 amount);
+    event Devest(address user, uint256 invested_amount, uint256 received_amount);
+    event FeeDeduction(address user, address strategist, uint256 fee_amount);
+
+    function invest(address) external payable;
+    function devest(address, uint256) external payable;
     function getRoyalty() external view returns(uint256);
+
 }
