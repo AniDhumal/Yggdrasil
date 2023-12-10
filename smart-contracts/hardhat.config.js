aa339599a50d5358927cf02153486e4b7b97e72b
epizeuxius <markericanthony@gmail.com>
2023-12-10 07:05:06 +0530

    Cleaned up hardhat config and some finishing touches

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,13 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 // NEVER record important private keys in your code - this is for demo purposes
-const GOERLI_TESTNET_PRIVATE_KEY =
-  "cbc44f3dfb4047eb4e86a48a3278a3f48bebe0f953bb65ddeb982faa5a3a8bca";
-const ARBITRUM_MAINNET_TEMPORARY_PRIVATE_KEY = "";
-const LINEA_TESTNET_PRIVATE_KEY =
-  "0xcbc44f3dfb4047eb4e86a48a3278a3f48bebe0f953bb65ddeb982faa5a3a8bca";
 
-const { INFURA_API_KEY } = process.env;
+const { PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -19,43 +14,38 @@ module.exports = {
     arbitrumGoerli: {
       url: "https://goerli-rollup.arbitrum.io/rpc",
       chainId: 421613,
-      accounts: [GOERLI_TESTNET_PRIVATE_KEY],
-    },
-    arbitrumOne: {
-      url: "https://arb1.arbitrum.io/rpc",
-      //accounts: [ARBITRUM_MAINNET_TEMPORARY_PRIVATE_KEY]
+      accounts: [PRIVATE_KEY],
     },
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
       chainId: 80001,
-      accounts: [GOERLI_TESTNET_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
     },
     linea_testnet: {
       url: "https://linea-goerli.infura.io/v3/371af9d712614049b443043a9202cd6e",
-      // chainId: 59140,
-      accounts: [LINEA_TESTNET_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
     },
     linea_mainnet: {
       url: "https://rpc.linea.build/",
-      accounts: [LINEA_TESTNET_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
     },
     scrollTestnet: {
       url: "https://sepolia-rpc.scroll.io",
-      accounts: [LINEA_TESTNET_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
       minGasPrice: 10000000,
     },
     zkEVM_testnet: {
       url: "https://rpc.public.zkevm-test.net",
-      accounts: [LINEA_TESTNET_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
     },
     base_goerli: {
       url: "https://goerli.base.org",
-      accounts: [LINEA_TESTNET_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
       gasPrice: 1000000000,
     },
     mantleTest: {
       url: "https://rpc.testnet.mantle.xyz", // testnet
-      accounts: [LINEA_TESTNET_PRIVATE_KEY ?? ""],
+      accounts: [PRIVATE_KEY ?? ""],
     },
     // etherscan: {
     //   apiKey: {
